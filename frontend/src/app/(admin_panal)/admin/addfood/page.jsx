d22823991dfc263989de30d3eab6123272657573
Somesh Khicher <someshkhicher111@gmail.com>
2026-05-27 08:37:22 +0530

    final addfood fix

--- frontend/src/app/(admin_panal)/admin/addfood/page.jsx
+++ frontend/src/app/(admin_panal)/admin/addfood/page.jsx
@@ -1,18 +1,3 @@
-"use client";
-
 export default function Page() {
-
-  return (
-
-    <div className="p-10">
-
-      <h1 className="text-4xl font-bold">
-
-        Add Food
-
-      </h1>
-
-    </div>
-
-  );
+  return <h1>Add Food</h1>;
 }
