34bbd8b2a504afda26351d6c0ebd845f208075eb
Somesh Khicher <someshkhicher111@gmail.com>
2026-05-27 08:47:35 +0530

    fix next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,42 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+
+//   images: {
+
+//     remotePatterns: [
+
+//       {
+
+//         protocol: "http",
+
+//         hostname:
+//           "localhost",
+
+//         port: "5000",
+
+//         pathname:
+//           "/uploads/**",
+
+//       },
+
+//     ],
+
+//   },
+
+// };
+
+// export default nextConfig;
+/**    
+ @type {import('next').NextConfig} */
 
 const nextConfig = {
+
+  turbopack: {
+
+    root: __dirname,
+
+  },
 
   images: {
 
@@ -14,6 +50,18 @@ const nextConfig = {
           "localhost",
 
         port: "5000",
+
+        pathname:
+          "/uploads/**",
+
+      },
+
+      {
+
+        protocol: "https",
+
+        hostname:
+          "foodie-qsnw.onrender.com",
 
         pathname:
           "/uploads/**",
