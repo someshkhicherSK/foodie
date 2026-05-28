de5a21faa262a1643a462afe36e0864409556073
Somesh Khicher <someshkhicher111@gmail.com>
2026-05-28 20:03:40 +0530

    fix profile page

---
+++ frontend/src/app/(user_panal)/profile/page.jsx
@@ -0,0 +1,16 @@
+export default function Page() {
+
+  return (
+
+    <div className="p-10">
+
+      <h1 className="text-4xl font-bold">
+
+        Profile Page
+
+      </h1>
+
+    </div>
+
+  );
+}
