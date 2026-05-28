d3d807fed20e285ad739807e0ced9b2c80da9857
Somesh Khicher <someshkhicher111@gmail.com>
2026-05-28 20:12:16 +0530

    add root homepage

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+
+  return (
+
+    <div className="min-h-screen flex items-center justify-center bg-orange-50">
+
+      <h1 className="text-5xl font-bold text-red-500">
+
+        Foodie App 🍔
+
+      </h1>
+
+    </div>
+
+  );
+}
